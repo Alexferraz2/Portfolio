ddaaa6d44b34804fcb8e13bd1c2ca1e3cd9b5ddf
Alexferraz2 <alex_ferraz2@hotmail.com>
2024-06-01 21:58:04 -0300

    Criado o arquivo _docuement para importar fontes e inserir o título da página

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,17 @@
+import Head from "next/head"
+
+
 import "../src/style.css"
 
 function App ({ Component, pageProps }) {
   return (
-    <Component {...pageProps} />
+    <>
+        <Head>
+            <title>Portfólio de Alexsandro</title>
+        </Head>
+        <Component {...pageProps} />
+    </>
+    
   )
 }
 
