1546fbdc5ce546b1283cc084299b25fa3c2c0f71
Alexferraz2 <alex_ferraz2@hotmail.com>
2025-10-24 23:00:50 -0300

    Adcionado logo das stacks

--- src/components/MyStack/MyStack.jsx
+++ src/components/MyStack/MyStack.jsx
@@ -4,35 +4,67 @@ import Text from "../Text/Text";
 import styles from "./MyStack.module.css"
 
 function MyStack() {
-    return(
+    return (
         <div className={styles.containerStack} id="stack">
             <div><SubTitle>Minhas Stack</SubTitle></div>
             <div className={styles.stack}>
                 <div className={styles.frontend}>
-                    <Text>FrontEnd</Text>
-                    <ul className={styles.lists}>
-                        <li>HTML</li>
-                        <li>CSS</li>
-                        <li>Javascript</li>
-                        <li>ReactJS</li>
-                        <li>NextJs</li>
-                        <li>Styled Components</li>
-                    </ul>
+                    <p className={styles.tittleStack}>FrontEnd</p>
+                    <div className={styles.imgcontainer}>
+                        <div>
+                            <img src="/HTML5.png" alt="html5" className={styles.logo} />
+                        </div>
+                        <div>
+                            <img src="/CSS3.png" alt="css3" className={styles.logo} />
+                        </div>
+                        <div >
+                            <img src="/tailwind.png" alt="tailwind" className={styles.logo} />
+                        </div>
+                        <div >
+                            <img src="/javascript.png" alt="javascript" className={styles.logo} />
+                        </div>
+                        <div >
+                            <img src="/Typescript.png" alt="Typescript" className={styles.logo} />
+                        </div>
+                        <div >
+                            <img src="/react.png" alt="react" className={styles.logo} />
+                        </div>
+                        <div>
+                            <img src="/Nextjs Logo.png" alt="next" className={styles.logo} />
+                        </div>
+                    </div>
+
                 </div>
                 <div className={styles.backend}>
-                    <Text>BackEnd</Text>
-                    <ul className={styles.lists}>
-                        <li>Javascript</li>
-                        <li>NodeJs</li>
-                        <li>NextJs</li>
-                        
-                        
-                    </ul>
+                    <p className={styles.tittleStack}>BackEnd</p>
+                    <div className={styles.imgcontainer}>
+                        <div className={styles.imgcontainer}>
+                            <img src="/nodejs.svg" alt="nodejs" className={styles.logo} />
+                        </div>
+                        <div className={styles.imgcontainer}>
+                            <img src="/MongoDB.png" alt="MongoDB" className={styles.logo} />
+                        </div>
+                        <div className={styles.imgcontainer}>
+                            <img src="/mysql.png" alt="mysql.png" className={styles.logo} />
+                        </div>
+                        <div className={styles.imgcontainer}>
+                            <img src="/postgresql.png" alt="postgresql.png" className={styles.logo} />
+                        </div>
+                        <div className={styles.imgcontainer}>
+                            <img src="/prisma.svg" alt="prisma" className={styles.logo} />
+                        </div>
+
+                        <div className={styles.imgcontainer}>
+                            <img src="/zod.png" alt="zod" className={styles.logo} />
+                        </div>
+                    </div>
+
+
                 </div>
             </div>
 
         </div>
-        
+
     )
 }
 
